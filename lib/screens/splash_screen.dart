aae116407c92f6077ac27d29aa0fa228d7b8791b
piro-piyush <piyush72717272@gmail.com>
2024-10-16 22:39:48 +0530

    first day commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Lottie.asset("assets/netflix.json"),
+    );
+  }
+}
