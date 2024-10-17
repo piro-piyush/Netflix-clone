08e880b3cd913806a7d6cb8ea72208907c5e9672
piro-piyush <piyush72717272@gmail.com>
2024-10-17 21:58:39 +0530

    added bottom nav bar

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+
+import '../widgets/bottom_nav_bar.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,6 +13,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => BottomNavBar()));
+    })
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
