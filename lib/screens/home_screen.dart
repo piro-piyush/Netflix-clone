3b349f3d3c4a9f7b07306369c25290e132fedc21
piro-piyush <piyush72717272@gmail.com>
2024-10-17 22:50:45 +0530

    utiles added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/common/utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,7 +11,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        title: Image.asset("assets/logo.png",
+        height: 50,
+        width: 120,),
+        actions: [
+          InkWell(
+            onTap: (){
+
+            },
+            child: Icon(Icons.search,size: 30,
+            color: Colors.white,),
+          ),
+          SizedBox(width: 20,),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: Colors.blue,
+              height: 27,
+              width: 27,
+              ),
+          ),
+          SizedBox(width: 20,)
+        ],
+        centerTitle: true,
+      ),
       body: Center(child: Text("Home Screen",style: TextStyle(color: Colors.white),),),
     );
   }
