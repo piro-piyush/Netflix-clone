daacc888766cf59ac59a9d91ddbf310fd6fc06d2
piro-piyush <piyush72717272@gmail.com>
2024-10-18 18:27:07 +0530

    Create upcoming movies and now playing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/common/utils.dart';
+import 'package:netflix_clone/models/upcoming_movie_model.dart';
+import 'package:netflix_clone/services/api_services.dart';
+import 'package:netflix_clone/widgets/movie_card_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,36 +12,65 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<UpcomingMovieModal> upcomingFuture;
+  late Future<UpcomingMovieModal> nowPlayingFuture;
+
+  ApiServices apiServices = ApiServices();
+
+  @override
+  void initState() {
+    nowPlayingFuture = apiServices.getNowPlayingMovies();
+    upcomingFuture = apiServices.getUpcomingMovies();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: kBackgroundColor,
-        title: Image.asset("assets/logo.png",
-        height: 50,
-        width: 120,),
-        actions: [
-          InkWell(
-            onTap: (){
-
-            },
-            child: Icon(Icons.search,size: 30,
-            color: Colors.white,),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: kBackgroundColor,
+          title: Image.asset(
+            "assets/logo.png",
+            height: 50,
+            width: 120,
           ),
-          SizedBox(width: 20,),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(6),
-            child: Container(
-              color: Colors.blue,
-              height: 27,
-              width: 27,
+          actions: [
+            InkWell(
+              onTap: () {},
+              child: Icon(
+                Icons.search,
+                size: 30,
+                color: Colors.white,
               ),
-          ),
-          SizedBox(width: 20,)
-        ],
-        // centerTitle: true,
-      ),
-      body: Center(child: Text("Home Screen",style: TextStyle(color: Colors.white),),),
-    );
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Container(
+                color: Colors.blue,
+                height: 27,
+                width: 27,
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            )
+          ],
+          // centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+                height:220,
+                child: MovieCardWidget(future: nowPlayingFuture, headLineText: "Now Playing")
+            ),
+            SizedBox(
+                height:220,
+                child: MovieCardWidget(future: upcomingFuture, headLineText: "Upcoming Movies")
+            )
+          ],
+        ));
   }
 }
