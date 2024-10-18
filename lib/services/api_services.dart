00a2b3f7e9054bfb2a1408a260435b512873c0f6
piro-piyush <piyush72717272@gmail.com>
2024-10-18 18:27:40 +0530

    nowplaying add

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,9 +1,36 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:netflix_clone/common/utils.dart';
+import 'package:netflix_clone/models/upcoming_movie_model.dart';
+import 'package:http/http.dart' as http;
 
 const  baseUrl = "https://api.themoviedb.org/3/";
 var key = "?api_key=$apiKey";
 late String endPoint;
 
 class ApiServices {
+  Future<UpcomingMovieModal> getUpcomingMovies() async {
+    endPoint = "movie/upcoming";
+    final url = "$baseUrl$endPoint$key";
 
+    final response  = await http.get(Uri.parse(url));
+    if(response.statusCode == 200){
+      log("Success");
+      return UpcomingMovieModal.fromJson(jsonDecode(response.body));
+    }
+    throw Exception("Failed to load upcoming movies");
+  }
+
+  Future<UpcomingMovieModal> getNowPlayingMovies() async {
+    endPoint = "movie/now_playing";
+    final url = "$baseUrl$endPoint$key";
+
+    final response  = await http.get(Uri.parse(url));
+    if(response.statusCode == 200){
+      log("Success");
+      return UpcomingMovieModal.fromJson(jsonDecode(response.body));
+    }
+    throw Exception("Failed to load Now Playing movies");
+  }
 }
